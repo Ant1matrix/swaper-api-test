117ab8b296ac76d9c09e3966f3842e433113be4a
Anton Aleksandrov <anton.aleksandrov@icloud.com>
2024-08-19 20:15:56 +0300

    Alocated all test realted files in a separate folder with proper hierarchy

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -3,12 +3,12 @@ require("dotenv").config();
 import { expectedResponse } from "./resources/expected.data";
 import { DataResponse, DataPost } from "./resources/data.types";
 
-const SWAPER_URL: string = 'https://swaper.com/';
+const SWAPER_URL: string = "https://swaper.com/";
 
 const user = {
   USERNAME: process.env.USERNAME ?? process.exit(),
-  PASSWORD: process.env.PASSWORD ?? process.exit()  
-}
+  PASSWORD: process.env.PASSWORD ?? process.exit(),
+};
 //Initialising global variables & interfaces
 
 const expectedKeys = Object.keys(expectedResponse) as Array<keyof DataResponse>;
@@ -18,17 +18,15 @@ const expectedKeys = Object.keys(expectedResponse) as Array<keyof DataResponse>;
 describe("Swaper API Test", () => {
   const { USERNAME, PASSWORD } = user;
 
-    const userLoginData: DataPost = {
-      name: USERNAME,
-      password: PASSWORD,
-    };
-    
-    //Creating axios instance
-    const axiosInit = axios.create({baseURL: SWAPER_URL})
-    
+  const userLoginData: DataPost = {
+    name: USERNAME,
+    password: PASSWORD,
+  };
+
+  //Creating axios instance
+  const axiosInit = axios.create({ baseURL: SWAPER_URL });
 
   it("should login to public REST endpoint and check that accountBalance is 0.0", async () => {
-
     const response = await axiosInit.post("rest/public/login", userLoginData);
     const data: DataResponse = response.data;
 
