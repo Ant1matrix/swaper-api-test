d3df1fdd7f58b854227fd65bc1d7b651d0c1f801
Anton Aleksandrov <anton.aleksandrov@icloud.com>
2024-08-19 17:27:45 +0300

    Implemented Node Env Variables and added accountBallance assertion

---
+++ resources/data.types.ts
@@ -0,0 +1,34 @@
+export interface DataResponse {
+    username: string;
+    permissions: null;
+    investorType: string;
+    registrationStep: string;
+    status: string;
+    firstName: string;
+    accountBalance: number;
+    unsettledBalance: number;
+    bankAccountNumbers: [];
+    number: string;
+    idDocumentsStatus: string;
+    displayPortfolio: boolean;
+    inUploadStep: boolean;
+    vip: boolean;
+    vipConfirmed: boolean;
+    notifications: { unreadPushNotifications: number };
+    currency: string;
+    language: string;
+    hasChangedCurrency: true;
+    registeredAt: string;
+    isShowPep: boolean;
+    isPepOrPrivateInvestorNotOwner: boolean;
+    isShowLastAgreementPopup: boolean;
+    isShowKycPopup: boolean;
+    isShowTwoFaPopup: boolean;
+    usingTwoFa: boolean;
+    updatePersonalData: boolean;
+    shouldUpdatePrivateInvestorBirthDate: boolean;
+    isShowPlAssignmentAgreementPopup: boolean;
+    isDeleteRequestAllowed: boolean;
+    isShowProductTypePopup: boolean;
+    id: number;
+  }
